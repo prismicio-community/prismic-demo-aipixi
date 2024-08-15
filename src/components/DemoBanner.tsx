d57d9b525998977e6bd03cd7d8ec11731b84e283
Angelo Ashmore <angeloashmore@users.noreply.github.com>
2024-08-14 15:05:12 -1000

    feat: hide first demo banner message

--- src/components/DemoBanner.tsx
+++ src/components/DemoBanner.tsx
@@ -4,12 +4,24 @@ import clsx from "clsx";
 import { ReactNode, useEffect, useState } from "react";
 
 export function DemoBanner() {
+	const [isShowing, setIsShowing] = useState(false);
+	const [isShowing2, setIsShowing2] = useState(false);
+
+	useEffect(() => {
+		const timers = [
+			setTimeout(() => setIsShowing(true), 1500),
+			setTimeout(() => setIsShowing2(true), 3500),
+			setTimeout(() => setIsShowing(false), 5500),
+		];
+		return () => timers.forEach((timer) => clearTimeout(timer));
+	}, []);
+
 	return (
 		<div className="pointer-events-none fixed left-0 right-0 top-0 z-20 p-8">
 			<Chat>
-				<ChatMessage delay={1500}>👋 Hey! How does it look?</ChatMessage>
-				<ChatMessage delay={3500}>
-					Head back to the demo tab when you&rsquo;re ready.{" "}
+				<ChatMessage show={isShowing}>👋 Hey! How does it look?</ChatMessage>
+				<ChatMessage show={isShowing2}>
+					Head back to the demo tab when you&rsquo;re ready
 				</ChatMessage>
 			</Chat>
 		</div>
@@ -26,25 +38,20 @@ function Chat(props: ChatProps) {
 	return <div className="flex flex-col items-center gap-2">{children}</div>;
 }
 
-type ChatBubbleProps = {
-	delay: number;
+type ChatMessageProps = {
+	show?: boolean;
+	// delay: number;
 	children?: ReactNode;
 };
 
-function ChatMessage(props: ChatBubbleProps) {
-	const { delay, children } = props;
-
-	const [isShowing, setIsShowing] = useState(false);
-	useEffect(() => {
-		const timeout = setTimeout(() => setIsShowing(true), delay);
-		return () => clearTimeout(timeout);
-	}, [delay]);
+function ChatMessage(props: ChatMessageProps) {
+	const { show = true, children } = props;
 
 	return (
 		<div
 			className={clsx(
-				"font-system pointer-events-auto flex flex-col gap-1 rounded-xl bg-neutral-900 px-6 py-4 text-center text-xl text-white shadow-md transition-[opacity,transform] transition-transform duration-200 dark:bg-white dark:text-neutral-900",
-				!isShowing && "-translate-y-4 scale-95 opacity-0",
+				"pointer-events-auto relative z-20 flex flex-col gap-1 rounded-xl bg-neutral-900 px-6 py-4 text-center font-system text-xl leading-7 text-white shadow-md transition-[opacity,transform,margin] duration-200 last:z-10 dark:bg-white dark:text-neutral-900",
+				!show && "-mt-[60px] origin-bottom scale-75 opacity-0 first:-mt-[68px]",
 			)}
 		>
 			{children}
