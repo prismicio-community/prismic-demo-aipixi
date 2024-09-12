219e977d56205cf01da126b1d4d12cf536b97e37
Angelo Ashmore <angeloashmore@users.noreply.github.com>
2024-09-11 16:54:36 -1000

    fix: clean up

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,4 @@
 import { Plus_Jakarta_Sans } from "next/font/google";
-import { ThemeProvider } from "next-themes";
 import { PrismicPreview } from "@prismicio/next";
 
 import { repositoryName } from "@/prismicio";
@@ -9,13 +8,6 @@ import { Header } from "@/components/Header";
 import { IconSymbols } from "@/components/Icon";
 
 import "./globals.css";
-
-const plusJakartaSans = Plus_Jakarta_Sans({
-	subsets: ["latin"],
-	weight: ["400", "600", "700"],
-	display: "swap",
-	variable: "--font-plus-jakarta-sans",
-});
 
 export default function RootLayout({
 	children,
@@ -27,15 +19,20 @@ export default function RootLayout({
 			suppressHydrationWarning
 		>
 			<body className="relative min-h-screen bg-neutral-100 leading-relaxed text-slate-800 dark:bg-slate-950 dark:text-white dark:antialiased">
-				<ThemeProvider attribute="class">
-					<Background />
-					<Header />
-					<main className="relative">{children}</main>
-					<Footer />
-				</ThemeProvider>
-				<PrismicPreview repositoryName={repositoryName} />
+				<Header />
+				<main>{children}</main>
+				<Footer />
+				<Background />
 				<IconSymbols />
 			</body>
+			<PrismicPreview repositoryName={repositoryName} />
 		</html>
 	);
 }
+
+const plusJakartaSans = Plus_Jakarta_Sans({
+	subsets: ["latin"],
+	weight: ["400", "600", "700"],
+	display: "swap",
+	variable: "--font-plus-jakarta-sans",
+});
